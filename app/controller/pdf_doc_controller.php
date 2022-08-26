d4034e5cf2cf0631d6b7757939885939c77b71af
henrygotmojo <henrygotmojo@gmail.com>
2022-08-26 10:17:23 +0800

    create dummy preview action

--- app/controller/pdf_doc_controller.php
+++ app/controller/pdf_doc_controller.php
@@ -3,34 +3,46 @@ F::redirect('auth&callback='.base64_encode($_SERVER['REQUEST_URI']), !Auth::user
 F::error('Forbidden', !Auth::userInRole('SUPER,ADMIN'));
 
 
-// extra buttons
-$xfa['preview'] = "{$fusebox->controller}.preview";
-$xfa['editLayout'] = 'pdf_row';
-
-
-// config
-$scaffold = array_merge([
-	'beanType' => 'pdfdoc',
-	'editMode' => 'inline',
-	'allowDelete' => Auth::userInRole('SUPER'),
-	'layoutPath' => F::appPath('view/pdf_doc/layout.php'),
-	'listOrder' => 'ORDER BY alias ',
-	'listField' => array(
-		'id' => '60',
-		'alias' => '15%',
-		'title|remark' => '40%',
-	),
-	'fieldConfig' => array(
-		'alias' => array('required' => true),
-		'title' => array('placeholder' => true, 'required' => true),
-		'remark' => array('format' => 'textarea', 'style' => 'height: 5rem', 'placeholder' => true),
-	),
-	'scriptPath' => array(
-		'row' => F::appPath('view/pdf_doc/row.php'),
-	),
-	'writeLog' => class_exists('Log'),
-], $pdfDocScaffold ?? $pdf_doc_scaffold ?? []);
-
-
 // run!
-include F::appPath('controller/scaffold_controller.php');
+switch ( $fusebox->action ) :
+
+
+	// view as pdf
+	case 'preview':
+		F::error('Argument [docID] is required', empty($arguments['docID']));
+var_dump($arguments);
+		break;
+
+
+	// crud operations
+	default:
+		// extra buttons
+		$xfa['preview'] = "{$fusebox->controller}.preview";
+		$xfa['editLayout'] = 'pdf_row';
+		// config
+		$scaffold = array_merge([
+			'beanType' => 'pdfdoc',
+			'editMode' => 'inline',
+			'allowDelete' => Auth::userInRole('SUPER'),
+			'layoutPath' => F::appPath('view/pdf_doc/layout.php'),
+			'listOrder' => 'ORDER BY alias ',
+			'listField' => array(
+				'id' => '60',
+				'alias' => '15%',
+				'title|remark' => '40%',
+			),
+			'fieldConfig' => array(
+				'alias' => array('required' => true),
+				'title' => array('placeholder' => true, 'required' => true),
+				'remark' => array('format' => 'textarea', 'style' => 'height: 5rem', 'placeholder' => true),
+			),
+			'scriptPath' => array(
+				'row' => F::appPath('view/pdf_doc/row.php'),
+			),
+			'writeLog' => class_exists('Log'),
+		], $pdfDocScaffold ?? $pdf_doc_scaffold ?? []);
+		// component
+		include F::appPath('controller/scaffold_controller.php');
+
+
+endswitch;
