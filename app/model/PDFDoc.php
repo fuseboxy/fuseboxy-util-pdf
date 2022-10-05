8e2266f49106069b78709c5b01ec02987807a28f
henrygotmojo <henrygotmojo@gmail.com>
2022-10-05 15:53:38 +0800

    create and apply pdfdoc-array method

--- app/model/PDFDoc.php
+++ app/model/PDFDoc.php
@@ -12,11 +12,71 @@ class PDFDoc {
 	/**
 	<fusedoc>
 		<description>
-			load specific pdf-doc record (when necessary)
+			load rows of specific pdf-doc record
+			===> convert to array data for util-array2html method
 		</description>
 		<io>
 			<in>
-				<mixed name=$doc" comments="id|alias|object" />
+				<mixed name="$doc" comments="id|alias|object" />
+			</in>
+			<out>
+				<array name="~return~">
+					<structure name="+">
+						<string name="type" default="div" value="div|p|h1|h2|h3|h4|h5|h6|small|ol|ul|br|hr|img|pagebreak" />
+						<!-- value -->
+						<string name="value" />
+						<!-- styling -->
+						<boolean name="bold" default="false" />
+						<boolean name="underline" default="false" />
+						<boolean name="italic" default="false" />
+						<string name="color" value="ffccaa|#ffccaa|.." />
+						<number name="size" optional="yes" oncondition="div|p|ul|ol|br" />
+						<!-- alignment -->
+						<string name="align" value="left|right|center|justify" oncondition="div|p|h1..h6|small|img" />
+						<!-- options -->
+						<number name="repeat" optional="yes" default="1" oncondition="br" />
+						<number name="height" optional="yes" oncondition="img" />
+						<number name="width" optional="yes" oncondition="img" />
+						<string name="url" optional="yes" />
+					</structure>
+				</array>
+			</out>
+		</io>
+	</fusedoc>
+	*/
+	public static function array($doc) {
+		$result = array();
+		// load record
+		$bean = self::load($doc);
+		if ( $bean === false ) return false;
+		// get related rows
+		$beanRows = ORM::get('pdfrow', 'disabled = 0 AND pdfdoc_id = ? ORDER BY IFNULL(seq, 9999), id ', array($bean->id));
+		if ( $beanRows === false ) {
+			self::$error = "[PDFDoc::array] Error loading PDF rows (docID={$bean->id})";
+			return false;
+		}
+		// transform each item from object to array
+		foreach ( $beanRows as $rowID => $rowItem ) {
+			$result[] = Bean::export($rowItem);
+			if ( $result[array_key_last($result)] === false ) {
+				self::$error = "[PDFDoc::array] Error exporting PDF row (rowID={$rowID})";
+				return false;
+			}
+		}
+		// done!
+		return $result;
+	}
+
+
+
+	/**
+	<fusedoc>
+		<description>
+			load specific pdf-doc record
+		</description>
+		<io>
+			<in>
+				<mixed name="$doc" comments="id|alias|object" />
 			</in>
 			<out>
 				<object name="~return~" type="pdfdoc" />
@@ -89,24 +149,9 @@ class PDFDoc {
 			self::$error = "[PDFDoc::render] Invalid format to render ({$format})";
 			return false;
 		}
-		// load record
-		$bean = self::load($doc);
-		if ( $bean === false ) return false;
-		// get related rows
-		$beanRows = ORM::get('pdfrow', 'disabled = 0 AND pdfdoc_id = ? ORDER BY IFNULL(seq, 9999), id ', array($bean->id));
-		if ( $beanRows === false ) {
-			self::$error = "[PDFDoc::render] Error loading PDF rows (docID={$bean->id})";
-			return false;
-		}
-		// transform each item from object to array
-		$data = array();
-		foreach ( $beanRows as $rowID => $rowItem ) {
-			$data[] = Bean::export($rowItem);
-			if ( $data[array_key_last($data)] === false ) {
-				self::$error = "[PDFDoc::render] Error exporting PDF row (rowID={$rowID})";
-				return false;
-			}
-		}
+		// load data
+		$data = self::array($doc);
+		if ( $data === false ) return false;
 		// display as pdf directly (or capture html output)
 		$result = ( $format == 'pdf' ) ? Util_PDF::array2pdf($data) : Util_PDF::array2html($data);
 		if ( $result === false ) {
