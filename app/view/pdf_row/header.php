f042d1988e9c20948f6190ca03839b7da396547c
henrygotmojo <henrygotmojo@gmail.com>
2022-11-10 11:48:03 +0800

    add buttons to control new row position

--- app/view/pdf_row/header.php
+++ app/view/pdf_row/header.php
@@ -18,32 +18,61 @@ include F::appPath('view/scaffold/header.php');
 $doc = Util::phpQuery(ob_get_clean());
 
 
-// custom set of new buttons
-$all = array(
-	['div','p','small'],
-	['h1','h2','h3','h4','h5','h6'],
-	['ul','ol'],
-	['img'],
-	['br','hr','pagebreak'],
-);
-ob_start();
+// button : new row
 if ( isset($xfa['new']) ) :
-	foreach ( $all as $group ) :
-		?><div class="btn-group ml-2"><?php
+	ob_start();
+	// new row position
+	$positions = array(
+		[
+			'btnText' => 'Add to Top',
+			'dataMode' => 'after',
+			'dataTarget' => '.scaffold-list > .scaffold-first-row',
+		],
+		[
+			'btnText' => 'Add to Bottom',
+			'dataMode' => 'before',
+			'dataTarget' => '.scaffold-list > .scaffold-last-row',
+		],
+	);
+	?><div class="btn-group btn-group-xs" role="group"><?php
+		foreach ( $positions as $i => $pos ) :
+			?><button 
+				type="button"
+				class="btn btn-outline-primary px-2 small <?php if ( $i == 0 ) echo 'active'; ?>"
+				onclick="
+					// mark selected
+					$(this).addClass('active').siblings().removeClass('active');
+					// change button target
+					$(this).closest('.scaffold-header').find('.btn-new').attr('data-mode', '<?php echo $pos['dataMode']; ?>').attr('data-target', '<?php echo $pos['dataTarget']; ?>');
+				"
+			><?php echo $pos['btnText']; ?></button><?php
+		endforeach;
+	?></div><?php
+	// different row types
+	$types = array(
+		['div','p','small'],
+		['h1','h2','h3','h4','h5','h6'],
+		['ul','ol'],
+		['img'],
+		['br','hr','pagebreak'],
+	);
+	foreach ( $types as $group ) :
+		?><div class="btn-group ml-2" role="group"><?php
 			foreach ( $group as $rowType ) :
 				?><a 
 					href="<?php echo F::url($xfa['new'].'&rowType='.$rowType); ?>"
-					class="btn btn-xs btn-light b-1"
+					class="btn btn-xs btn-light b-1 btn-new"
 					data-toggle="ajax-load"
-					data-mode="before"
+					data-mode="after"
 					data-overlay="none"
-					data-target=".scaffold-list > .scaffold-last-row"
+					data-target=".scaffold-list > .scaffold-first-row"
 				><span class="px-2 mx-1"><?php echo $rowType; ?></span></a><?php
 			endforeach; // foreach-type
 		?></div><?php
 	endforeach; // foreach-group
+	// put into header
+	$doc->find('th.col-button')->addClass('text-nowrap')->html(ob_get_clean());
 endif;
-$doc->find('th.col-button')->addClass('text-nowrap')->html(ob_get_clean());
 
 
 // display
