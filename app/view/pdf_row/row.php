8a6d0b4d05de66710f39876edacb62c165d1583c
henrygotmojo <henrygotmojo@gmail.com>
2022-11-29 18:29:39 +0800

    fix bug of image overflow table cell

--- app/view/pdf_row/row.php
+++ app/view/pdf_row/row.php
@@ -30,16 +30,6 @@ include F::appPath('view/scaffold/row.php');
 $doc = Util::phpQuery(ob_get_clean());
 
 
-// preview row value
-$doc->find('div.col-value')->html(( $bean->type == 'pagebreak' ) ? '
-	<div class="row op-60">
-		<div class="col pr-0"><hr class="b-secondary" style="border-style: dashed;" /></div>
-		<div class="col-1 p-0 text-center text-muted small" style="line-height: 1.75rem;"><b>PAGEBREAK</b></div>
-		<div class="col pl-0"><hr class="b-secondary" style="border-style: dashed;" /></div>
-	</div>
-' : Util_PDF::array2html([ $bean->export() ]));
-
-
 // add paper border
 $doc->find('td.col-value')->addClass('bx-1 by-0 b-dark px-5');
 
@@ -47,6 +37,30 @@ $doc->find('td.col-value')->addClass('bx-1 by-0 b-dark px-5');
 // row type
 $doc->find('div.col-type')->addClass('badge badge-light b-1 text-uppercase ml-1');
 if ( $bean->type == 'br' ) $doc->find('div.col-type')->after('<small class="text-muted">('.$bean->value.')');
+
+
+// preview pagebreak
+if ( $bean->type == 'pagebreak') :
+	ob_start();
+	?><div class="row op-60">
+		<div class="col pr-0"><hr class="b-secondary" style="border-style: dashed;" /></div>
+		<div class="col-1 p-0 text-center text-muted small" style="line-height: 1.75rem;"><b>PAGEBREAK</b></div>
+		<div class="col pl-0"><hr class="b-secondary" style="border-style: dashed;" /></div>
+	</div><?php
+	$doc->find('div.col-value')->html(ob_get_clean());
+// preview others
+else :
+	$doc->find('div.col-value')->html( Util_PDF::array2html([ Bean::export($bean) ]));
+endif;
+
+
+// prevent row content overflow
+// ===> phpQuery has bug in css() method
+// ===> use attr(style) method instead...
+$tableStyle = $doc->find('table')->attr('style');
+$divStyle = $doc->find('div.col-value')->attr('style');
+$doc->find('table')->attr('style', 'table-layout:fixed;'.$tableStyle);
+$doc->find('div.col-value')->attr('style', 'overflow:hidden;'.$divStyle);
 
 
 // done!
